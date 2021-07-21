0e77bbb76e2a8da8ae44129724f69283b4062059
i <i@i-PC>
2021-07-21 13:21:42 +0530

    commit inside feature branch

---
+++ TestProject/src/ClassThree.java
@@ -0,0 +1,14 @@
+
+public class ClassThree {
+
+	public void subs() {
+		int x=21;
+		int y=2;
+		x=x+y;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
